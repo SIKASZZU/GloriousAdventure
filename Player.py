eabfff55d6f0eb5022e6dc037d8b8e1dd7c710c6
Oljake <109909586+Oljake@users.noreply.github.com>
2023-08-13 13:32:17 +0300

    Update Player.py

--- Player.py
+++ Player.py
@@ -1,5 +1,20 @@
 class Player:
-    def __init__(self, name, health, animation):
+    def __init__(self, name, health=100, stamina=100):
         self.name = name
         self.health = health
-        self.animation = animation
+        self.stamina = stamina
+        self.current_tile = 0  # Default tile
+        
+    def move_to_tile(self, tile_number):
+        self.current_tile = tile_number
+        if self.current_tile == 4:  # Tile with water
+            self.swimming()
+
+    def swimming(self):
+        if self.stamina == 0:
+            self.health -= 5
+        else:
+            self.stamina -= 10
+
+        if self.health <= 0:
+            print(f"{self.name} has died!")
