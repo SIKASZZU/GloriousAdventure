8b57a1ea61e9751aa899ec88d32a8e192697fed4
Oljake <109909586+Oljake@users.noreply.github.com>
2023-08-14 22:34:47 +0300

    Update Player.py
    Eraldi fail ja game failist player data ära kustutada

--- Player.py
+++ Player.py
@@ -1,20 +1,10 @@
-class Player:
-    def __init__(self, name, health=100, stamina=100):
-        self.name = name
+# Mind tuleb importida nii:
+# from player import user
+
+class Player():
+    def __init__(self, health, stamina, speed):
         self.health = health
         self.stamina = stamina
-        self.current_tile = 0  # Default tile
-        
-    def move_to_tile(self, tile_number):
-        self.current_tile = tile_number
-        if self.current_tile == 4:  # Tile with water
-            self.swimming()
+        self.speed = speed
 
-    def swimming(self):
-        if self.stamina == 0:
-            self.health -= 5
-        else:
-            self.stamina -= 10
-
-        if self.health <= 0:
-            print(f"{self.name} has died!")
+user = Player(health=10, stamina=10, speed=4)
