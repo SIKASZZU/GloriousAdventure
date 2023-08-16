2c6e918d3df356d505e99786f2466fae355f657a
Oljake <109909586+Oljake@users.noreply.github.com>
2023-08-16 22:21:41 +0300

    pole vaja

--- Enemy.py
+++
@@ -1,9 +0,0 @@
-
-
-class Enemy:
-    def __init__(self, types, health, speed):
-        self.types = types
-        self.health = health
-        self.speed = speed
-
-type = Enemy(types=type, health=2, speed= 1.5)
