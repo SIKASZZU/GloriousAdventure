24aa577a82553ad9b44a609e534309504eebc5f0
Oljake <109909586+Oljake@users.noreply.github.com>
2023-08-16 22:53:02 +0300

    Update game_entities.py

--- game_entities.py
+++ game_entities.py
@@ -6,6 +6,25 @@ import time
 
 
 class Player:
+    
+    """
+    Initialize a Player object with various attributes related to health, stamina, and speed.
+
+    Args:
+        max_health (float): The maximum health value for the player.
+        min_health (float): The minimum health value for the player.
+        health_regeneration_rate (float): The rate at which health regenerates for the player.
+
+        max_stamina (float): The maximum stamina value for the player.
+        min_stamina (float): The minimum stamina value for the player.
+        stamina_regeneration_rate (float): The rate at which stamina regenerates for the player.
+        stamina_degeneration_rate (float): The rate at which stamina decreases over time.
+
+        base_speed (float): The base speed of the player.
+        max_speed (float): The maximum speed the player can achieve.
+        min_speed (float): The minimum speed the player can have.
+    """
+        
     def __init__(self, max_health, min_health, health_regeneration_rate,
                  max_stamina, min_stamina, stamina_regeneration_rate, stamina_degeneration_rate,
                  base_speed, max_speed, min_speed):
