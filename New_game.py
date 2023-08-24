f2fe039f2228825c8263a76a81c3d24c53abbbcf
Oljake <109909586+Oljake@users.noreply.github.com>
2023-08-24 18:49:24 +0300

    Piltide pask peaks olema korras

--- New_game.py
+++ New_game.py
@@ -30,20 +30,23 @@ class Game:
         )
 
         # Game-related attributes
-        self.block_size = 25
+        self.block_size = 50
         self.player_color = 'red'
         self.REGENERATION_DELAY = 2
         self.stamina_regeneration_timer = 0
         self.base_speed = 4
 
         # Inventory display settings
-        # Create inventory display rects
         self.inventory_display_rects = [
             pygame.Rect(50, 50, 50, 50),
             pygame.Rect(100, 50, 50, 50),
             pygame.Rect(150, 50, 50, 50),
             pygame.Rect(200, 50, 50, 50),
             pygame.Rect(250, 50, 50, 50),
+            pygame.Rect(300, 50, 50, 50),
+            pygame.Rect(350, 50, 50, 50),
+            pygame.Rect(400, 50, 50, 50),
+            pygame.Rect(450, 50, 50, 50),
         ]
 
         self.inventory = {}  # Terve inv (prindi seda ja saad teada mis invis on)
@@ -52,7 +55,31 @@ class Game:
         self.item_images = {
             "Tree": pygame.image.load("images/Tree.PNG"),
             "Stone": pygame.image.load("images/Stone.PNG"),
+            "Rock": pygame.image.load("images/Rock.PNG"),
+            "Water": pygame.image.load("images/Water.PNG"),
         }
+
+        self.land_images = {"Ground_0": pygame.image.load("images/Ground/Ground_0.png"),
+                            "Ground_1": pygame.image.load("images/Ground/Ground_1.png"),
+                            "Ground_2": pygame.image.load("images/Ground/Ground_2.png"),
+                            "Ground_3": pygame.image.load("images/Ground/Ground_3.png"),
+                            "Ground_4": pygame.image.load("images/Ground/Ground_4.png"),
+                            "Ground_5": pygame.image.load("images/Ground/Ground_5.png"),
+                            "Ground_6": pygame.image.load("images/Ground/Ground_6.png"),
+                            "Ground_7": pygame.image.load("images/Ground/Ground_7.png"),
+                            "Ground_8": pygame.image.load("images/Ground/Ground_8.png"),
+                            "Ground_9": pygame.image.load("images/Ground/Ground_9.png"),
+                            "Ground_10": pygame.image.load("images/Ground/Ground_10.png"),
+                            "Ground_11": pygame.image.load("images/Ground/Ground_11.png"),
+                            "Ground_12": pygame.image.load("images/Ground/Ground_12.png"),
+                            "Ground_13": pygame.image.load("images/Ground/Ground_13.png"),
+                            "Ground_14": pygame.image.load("images/Ground/Ground_14.png"),
+                            "Ground_15": pygame.image.load("images/Ground/Ground_15.png"),
+                            "Ground_16": pygame.image.load("images/Ground/Ground_16.png"),
+                            "Ground_17": pygame.image.load("images/Ground/Ground_17.png"),
+                            "Ground_18": pygame.image.load("images/Ground/Ground_18.png"),
+                            "Ground_19": pygame.image.load("images/Ground/Ground_19.png"),
+                            }
 
         self.X_max = 1500 // self.block_size
         self.Y_max = 1500 // self.block_size
@@ -108,9 +135,10 @@ class Game:
             self.stamina_rect_bg = pygame.Rect(self.half_w - (self.stamina_bar_size_bg / 2) - 6, self.screen_y - 25, self.stamina_bar_size_bg + 12, 15)  # Kui staminat kulub, ss on background taga
             self.stamina_rect_border = pygame.Rect(self.half_w - (self.stamina_bar_size_border / 2) - 6, self.screen_y - 25, self.stamina_bar_size_border + 12, 15)  # K6igi stamina baride ymber border
             self.stamina_rect = pygame.Rect(self.half_w - (self.stamina_bar_size / 2) - 6, self.screen_y - 25, self.stamina_bar_size + 12, 15)
+
     def render_inventory(self):
         # Invi hall taust
-        inventory_bar_rect = pygame.Rect(50, 50, 250, 50)
+        inventory_bar_rect = pygame.Rect(50, 50, 450, 50)
         pygame.draw.rect(self.screen, '#B1B1B1', inventory_bar_rect)
 
         # Mustad boxid itemite ümber
@@ -139,26 +167,41 @@ class Game:
             text_rect = text.get_rect(center=(rect.x+10, rect.y+10))
             self.screen.blit(text, text_rect)
 
-        inventory_bar_rect = pygame.Rect(50, 50, 250, 50)
+        inventory_bar_rect = pygame.Rect(50, 50, 450, 50)
         pygame.draw.rect(self.screen, 'black', inventory_bar_rect, 4)  # Paksem border
 
-    # Koostab islandi
+    # Koostab uue saare
     def new_island(self, seed):
+        self.generated_ground_images = {}
 
-        # Mapile tekib seed nagu Minecraftis vms
+        # Seadistab juhuarvu genereerija seediga
         random.seed(seed)
         for x in range(self.X_max):
             for y in range(self.Y_max):
-                distance_to_center = ((x - self.center_x) ** 2 + (y - self.center_y) ** 2) ** 0.5  # Euclidean forumla
-                normalized_distance = distance_to_center / self.max_distance  # Output 0 kuni 1
-                land_probability = 1 - (normalized_distance ** 213)  # Suurendab (1) v6imalust tekkida mapi keskele.
-                if random.random() < land_probability:  # random.random output = [0, 1]
+                # Leiab kauguse keskpunktist
+                distance_to_center = ((x - self.center_x) ** 2 + (y - self.center_y) ** 2) ** 0.5
+                normalized_distance = distance_to_center / self.max_distance
+                land_probability = 1 - (normalized_distance ** 213)
+
+                # Määrab pinnase maapinnaks, kui juhuslik arv on väiksem kui maapinna tõenäosus
+                if random.random() < land_probability:
                     self.terrain_data[x][y] = 1
 
+                    # Määrab juhusliku pinnase pildi genereeritud maapinnatüübile
+                    if not self.generated_ground_images.get((x, y)):
+                        ground_image_name = f"Ground_{random.randint(0, 19)}"
+                        ground_image = self.land_images.get(ground_image_name)
+                        self.generated_ground_images[(x, y)] = ground_image
+
+        # Genereerib kivid ja puud
         for i in range(len(self.terrain_data)):
             for j in range(len(self.terrain_data[i])):
-                if self.terrain_data[i][j] == 1 and random.random() < 0.03:
-                    self.terrain_data[i][j] = 2
+                if self.terrain_data[i][j] == 1:
+                    # Kontrollib, kas lahter peaks olema kivi või puu
+                    if random.random() < 0.03:
+                        self.terrain_data[i][j] = 2  # Kivi
+                    elif random.random() < 0.04:
+                        self.terrain_data[i][j] = 4  # Puu
 
     def update_player(self):
         keys = pygame.key.get_pressed()
@@ -257,35 +300,48 @@ class Game:
         self.offset_x = self.camera_borders['left'] - self.camera_rect.left
         self.offset_y = self.camera_borders['top'] - self.camera_rect.top
 
-    # värvib ära teatud ruudud || 2 = rock, 1 = terrain (muru), 0 = water
     def render(self):
-        self.screen.fill('blue')  # Teeb ülejäänud backgroundi siniseks
+        # Tühjendab ekraani siniseks
+        self.screen.fill('blue')
 
+        # Joonistab maastiku
         for i in range(len(self.terrain_data)):
             for j in range(len(self.terrain_data[i])):
-                cell_color = 'blue'  # Default värv
+                terrain_x = j * self.block_size + self.offset_x
+                terrain_y = i * self.block_size + self.offset_y
 
-                if self.terrain_data[i][j] == 1:
-                    cell_color = 'green'
-                elif self.terrain_data[i][j] == 2:
-                    cell_color = 'gray'
+                # Joonistab maapinna
+                ground_image = self.generated_ground_images.get((i, j))
+                if ground_image:
+                    ground_image = pygame.transform.scale(ground_image, (self.block_size, self.block_size))
+                    self.screen.blit(ground_image, (terrain_x, terrain_y))
 
-                terrain_rect = pygame.Rect(
-                    j * self.block_size + self.offset_x,
-                    i * self.block_size + self.offset_y,
-                    self.block_size,
-                    self.block_size
-                )
+                # Joonistab objekte, kui andmetel pole väärtus 0 (vesi)
+                if self.terrain_data[i][j] != 0:
+                    item_image = None
+                    if self.terrain_data[i][j] == 2:
+                        item_image = self.item_images.get("Rock")
+                    elif self.terrain_data[i][j] == 4:
+                        item_image = self.item_images.get("Tree")
+                        if item_image:
+                            # Suurendab puu suurust
+                            item_image = pygame.transform.scale(item_image, (self.block_size * 2, self.block_size * 2))
+                            self.screen.blit(item_image, (terrain_x - self.block_size, terrain_y - self.block_size))
+                            continue  # Jätkab järgmise ploki renderdamist, kui puu on suurendatud
 
-                pygame.draw.rect(self.screen, cell_color, terrain_rect)
+                    if item_image is not None:
+                        item_image = pygame.transform.scale(item_image, (self.block_size, self.block_size))
+                        self.screen.blit(item_image, (terrain_x, terrain_y))
 
+        # Korrigeerib mängija suurust ja asukohta vastavalt kaamerale
         player_rect_adjusted = pygame.Rect(
             self.player_rect.left + self.offset_x,
             self.player_rect.top + self.offset_y,
-            self.block_size,
-            self.block_size,
+            self.block_size / 2,
+            self.block_size / 2,
         )
 
+        # Joonistab stamina ribad ja mängija asukoha markeri
         if self.stamina_bar_decay < 50:
             pygame.draw.rect(self.screen, '#F7F7F6', self.stamina_rect_bg, 0, 7)
             pygame.draw.rect(self.screen, '#4169E1', self.stamina_rect, 0, 7)
@@ -294,8 +350,10 @@ class Game:
         pygame.draw.rect(self.screen, 'yellow', self.interaction_rect, 2)
         pygame.draw.rect(self.screen, self.player_color, player_rect_adjusted)
 
-        self.render_inventory()  # Teeb inventory nähtavaks
+        # Renderdab inventuuri
+        self.render_inventory()
 
+        # Värskendab ekraani ja hoiab mängu kiirust 60 kaadrit sekundis
         pygame.display.flip()
         self.set_frame_rate.tick(60)
 
