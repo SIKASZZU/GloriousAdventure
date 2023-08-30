b37869ac239f3fccc9f7aca148d9f32a253853b6
SIKASZZU <109911099+SIKASZZU@users.noreply.github.com>
2023-08-30 16:37:28 +0300

    StaminaComponent lendas nahhuj stamina faili

--- components.py
+++ components.py
@@ -45,62 +45,6 @@ class HealthComponent:
     def get_health(self):
         return self.current_health
 
-
-class StaminaComponent:
-
-    """
-    Represents a stamina component that manages the stamina of an object.
-
-    Correct usage:
-      StaminaComponent(max_stamina, min_stamina, stamina_regeneration_rate, degeneration_rate)
-        OBJECT.command(*args, **kwargs)
-
-    'use_stamina(self, amount):'
-      Use a specified amount of stamina. Decreases the current stamina and updates the last update time
-      for regeneration calculations.
-
-    'stamina_regenerate(self):'
-      Adds a specified amount of stamina. Increases the current stamina and updates the last update time
-      for regeneration calculations.
-
-    'get_stamina(self):'
-      Retrieve the current stamina value while applying regeneration if needed.
-
-    Args:
-      max_stamina (float): The maximum stamina value the component can hold.
-      min_stamina (float): The minimum stamina value the component can have.
-    """
-
-    def __init__(self, max_stamina, min_stamina):
-        self.max_stamina = max_stamina
-        self.min_stamina = min_stamina
-        self.current_stamina = max(min_stamina, min(max_stamina, max_stamina))
-        self.stamina_last_update_time = time.time()
-        self.timer = 0
-        self.timer_regen = 0
-
-    def use_stamina(self, amount):
-        self.current_stamina = round(max(self.current_stamina - amount, self.min_stamina), 3)
-        self.stamina_last_update_time = time.time()
-        self.timer = 0
-
-    def stamina_regenerate(self, amount):
-        if not self.current_stamina >= self.max_stamina:
-            self.timer += 1
-            if self.timer >= 60:
-                self.timer_regen += 1
-                if self.timer_regen >= 3:
-                    self.current_stamina = round(self.current_stamina + amount, 3)
-                    self.timer_regen = 0
-                    if self.current_stamina == self.max_stamina:
-                        self.timer = 0
-        else:
-            self.max_stamina = 20
-
-    def get_stamina(self):
-        return self.current_stamina
-
-
 class SpeedComponent:
 
     """
