1ac7cdcf4b54163609ef80b2d07f9d83dfb7b658
SIKASZZU <109911099+SIKASZZU@users.noreply.github.com>
2023-09-02 19:27:46 +0300

    !Camera works

--- camera.py
+++ camera.py
@@ -1,19 +1,5 @@
-import pygame
-
-# Teeb boxi, kui minna sellele vastu, siis liigub kaamera
-    # Teeb boxi, kui minna sellele vastu, siis liigub kaamera
 def box_target_camera(self):
-    # camera stuff
-    self.camera_borders = {'left': 100, 'right': 100, 'top': 100, 'bottom': 100}
-    l = self.camera_borders['left']
-    t = self.camera_borders['top']
-    w = self.screen.get_size()[0] - (self.camera_borders['left'] + self.camera_borders['right'])
-    h = self.screen.get_size()[1] - (self.camera_borders['top'] + self.camera_borders['bottom'])
-    self.camera_rect = pygame.Rect(l, t, w, h)
-
-    # camera offset
-    self.offset_x = 0
-    self.offset_y = 0
+    '''Teeb boxi, kui minna sellele vastu, siis liigub kaamera'''
 
     if self.player_rect.left < self.camera_rect.left:
         self.camera_rect.left = self.player_rect.left
