f31113adbaa68975a76ed1c3cb8d03262b187b1f
SIKASZZU <soots.sigvrid@gmail.com>
2023-09-30 10:45:09 +0300

    hitboxi collision broken

--- collisions.py
+++ collisions.py
@@ -8,21 +8,20 @@ def check_collisions(self) -> None:
     for hit_box_x, hit_box_y, hit_box_width, hit_box_height, object_id, hit_box_offset_x, hit_box_offset_y in self.hit_boxes:
 
         # See mis listis on, seda on vaja, et see listist ära võtta, ära võttes kaob see mapi pealt ära
-        obj_hit_box = (
-        hit_box_x, hit_box_y, hit_box_width, hit_box_height, object_id, hit_box_offset_x, hit_box_offset_y)
+        obj_hit_box = (hit_box_x, hit_box_y, hit_box_width, hit_box_height, object_id, hit_box_offset_x, hit_box_offset_y)
 
         block_size: int = self.block_size
         terrain_x: int = hit_box_x - hit_box_offset_x
         terrain_y: int = hit_box_y - hit_box_offset_y
 
         try:
-            if object_id == 4:
+            if object_id == 4: 
                 block_size: int = self.block_size * 2
-            collision_terrain_rect = pygame.Rect(terrain_x, terrain_y, block_size , block_size)
+            collision_object_rect = pygame.Rect(terrain_x, terrain_y, block_size , block_size)
         except TypeError:
             pass
-
-        if self.player_rect.colliderect(collision_terrain_rect):
+        
+        if self.player_rect.colliderect(collision_object_rect):
             if keys[pygame.K_SPACE]:
 
                 # Et visuaalselt võtaks puu ära
@@ -30,11 +29,24 @@ def check_collisions(self) -> None:
                     terrain_x = terrain_x - self.block_size / 2
                     terrain_y = terrain_y - self.block_size
 
-
-                objects.remove_object_at_position(self, terrain_x, terrain_y,
-                                                  object_id)  # removib itemi maailmast nahhuj
+                objects.remove_object_at_position(self, terrain_x, terrain_y, object_id)  # removib itemi maailmast nahhuj
                 objects.add_object_to_inv(self, object_id, obj_hit_box)
+        
+### BROKEN
 
+#        collision_object_hitbox = pygame.Rect(hit_box_x, hit_box_y, hit_box_width, hit_box_height)
+#
+#        if self.player_rect.colliderect(collision_object_hitbox):
+#            print('collision')
+#            if self.player_x < hit_box_x:
+#                self.player_x -= 30
+#            if self.player_x > (hit_box_x + hit_box_width):
+#                self.player_x += 30
+#
+#            if self.player_y < hit_box_y:
+#                self.player_y -= 30
+#            if self.player_y > (hit_box_y + hit_box_height):
+#                self.player_y += 30
 
 def collison_terrain(self) -> None:
     render_range: int = self.render_range
