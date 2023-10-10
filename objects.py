6f1b5104100c423b7c0519e62f4a9f912409e616
Oljake <olariviitak@gmail.com>
2023-10-10 20:25:08 +0300

    objects / collisions / render  - kõiks on items_list'iga ära ühendatud

--- objects.py
+++ objects.py
@@ -1,5 +1,5 @@
 import pygame
-from items import minerals
+from items import items_list
 from images import ground_images, water_images, item_images
 
 class Object_Management:
@@ -39,94 +39,72 @@ class Object_Management:
     # 4   - objecti ID
     # 80  - hitboxi offset x
     # 40  - hitboxi offset y
-    def add_object_to_inv(
-            self, object_id: int, obj_hit_box: tuple[int, ...]
-            ) -> None:  # Tuple kus on ainult integer'id
-
+    def add_object_to_inv(self, object_id: int, obj_hit_box: tuple[int, ...]) -> None:
         # Hoiab leitud esemeid: test_found = ["test0", "test1", "test2"]
         items_found: set[str] = set()
-        # Hoiab leitud esemeid koos kogusega: test_count = {["Test0": 2], ["Test1": 4], ["Test2": 6]}
+        # Hoiab leitud esemeid koos kogusega: test_count = {"Test0": 2, "Test1": 4, "Test2": 6}
         item_count: dict[str, int] = {}
-    
+
         # itemi lisamine playeri inventorisse
-        for item_name, item_values in minerals.items():
-            if object_id == item_values[2]:
-                print(object_id, item_values[2])
-                items_found.add(item_name)
-    
+        for item_data in items_list:
+            if object_id == item_data["ID"]:
+                items_found.add(item_data["Name"])
+
         # Copy, et vältida erroreid, mis tulenevad suuruse muutumisega
         # Hoiab leitud esemeid: test_found_copy = ["test0", "test1", "test2"]
         items_found_copy: set[str] = items_found.copy()
-    
+
         try:
             for item_name in items_found_copy:
                 item_count[item_name] = 0
-    
-                for item_name, item_values in minerals.items():
-                    if object_id == item_values[2] and item_name in items_found:
-                    
-                        item_count[item_name] += 1
-                        items_found.remove(item_name)
-    
+
+                for item_data in items_list:
+                    if object_id == item_data["ID"] and item_data["Name"] in items_found:
+                        item_count[item_data["Name"]] += 1
+                        items_found.remove(item_data["Name"])
+
                         # Uuenda mängija inventori
-                        if item_name in self.inventory:
+                        if item_data["Name"] in self.inventory:
                             # Kui ese on juba inventoris, suurendab eseme kogust
-                            self.inventory[item_name] += 1
+                            self.inventory[item_data["Name"]] += 1
                         else:
                             # Kui tegemist on uue esemega, lisab selle inventori ja annab talle koguse: 1
-                            self.inventory[item_name] = 1
-    
+                            self.inventory[item_data["Name"]] = 1
+
                         index = self.hit_boxes.index(obj_hit_box)
                         self.hit_boxes.pop(index)
-    
-        except RuntimeError as e: print("\nError in file: objects.py", e)
-    
+
+        except RuntimeError as e:
+            print("\nError in file: objects.py", e)
 
     def place_and_render_object(self) -> None:
         keys = pygame.key.get_pressed()
 
         """Visuaalselt paneb objekti maailma (image)"""
+        # Object id, pilt, ja pildi suurus
+        interaction_boxes = {}
+
         for hit_box_x, hit_box_y, hit_box_width, hit_box_height, object_id, hit_box_offset_x, hit_box_offset_y in self.hit_boxes:
-            obj_image = None
-            
+            object_image = None
+
             terrain_x: int = (hit_box_x - hit_box_offset_x) + self.offset_x
             terrain_y: int = (hit_box_y - hit_box_offset_y) + self.offset_y
 
-            """SEE TULEB TEHA LISTI PEALE YMBER"""
+            for item in items_list:
+                if item.get("Type") == "Object" and item.get("ID") == object_id:
+                    object_image = item_images.get(item.get("Name"))
+                    object_width = item.get("Object_width")
+                    object_height = item.get("Object_height")
 
+                    interaction_boxes[object_id] = (object_image, object_width, object_height)
 
-            # Object id, pilt, ja pildi suurus
-            if object_id == 2:
-                obj_image = item_images.get("Rock")
-                object_width = int(self.block_size * 1)
-                object_height = int(self.block_size * 0.8)
-
-            elif object_id == 4:
-                obj_image = item_images.get("Tree")
-                object_width = int(self.block_size * 2)
-                object_height = int(self.block_size * 2)
-
-            elif object_id == 5:
-                obj_image = item_images.get("Flower")
-                object_width = int(self.block_size * 0.5)
-                object_height = int(self.block_size * 0.5)
-
-            elif object_id == 6:
-                obj_image = item_images.get("Mushroom")
-                object_width = int(self.block_size * 0.3)
-                object_height = int(self.block_size * 0.3)
-
-            else:
-                object_width = int(self.block_size)
-                object_height = int(self.block_size)
-
-            if obj_image:
+            if object_image:
                 position: tuple = (terrain_x, terrain_y)
                 object_rect = pygame.Rect(terrain_x, terrain_y, object_width, object_height)
-    
+
                 # Muudab pildi suurust ja visualiseerib seda
-                scaled_obj_image = pygame.transform.scale(obj_image, (object_width, object_height))
-                self.screen.blit(scaled_obj_image, position)
+                scaled_object_image = pygame.transform.scale(object_image, (object_width, object_height))
+                self.screen.blit(scaled_object_image, position)
 
             else: pass  # print('Object image missing!. File: objects.py Function: place_and_render_object')
 
