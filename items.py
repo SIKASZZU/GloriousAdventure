97762292da6f3e85cf070f265d5c725bb9d7a8f1
Oljake <olariviitak@gmail.com>
2023-10-10 15:25:36 +0300

    item list ja render objectite spawnimine listi järgi, objects.py on veel vaja ka fixida selle listiga ära

--- items.py
+++ items.py
@@ -1,17 +1,5 @@
 from images import item_images
 
-object_nr_list: list = [2, 4, 5, 6]  # object_id list
-
-
-# Item name, max hp, min hp, ITEM ID, breakable/pickable
-minerals = {"Tree":     (5, 0, 4, True),
-            "Stone":    (0, 0, 2, True),
-            "Flower":   (0, 0, 5, True),
-            "Mushroom": (0, 0, 6, True),
-            "Pickaxe":  (0, 0, 10, False),
-
-            }
-
 # Max hp, Min hp, Value, Image
 # b - blocks
 # m - minerals
@@ -24,3 +12,102 @@ minerals = {"Tree":     (5, 0, 4, True),
 #              "Axe":     (0, 0, "i_0002", item_images.get("Axe")),
 #              "Apple":   (0, 0, "c_0001", item_images.get("Apple")),
 #              }
+
+object_nr_list: list = [2, 4, 5, 6]  # object_id list
+
+
+# Item name, max hp, min hp, ITEM ID, breakable/pickable
+minerals = {"Tree":     (5, 0, 4, True),
+            "Stone":    (0, 0, 2, True),
+            "Flower":   (0, 0, 5, True),
+            "Mushroom": (0, 0, 6, True),
+            "Pickaxe":  (0, 0, 10, False),
+
+            }
+
+
+
+block_size = 100
+
+# hitbox: [offset_x, offset_y, width, height]
+items_list = [
+    {
+        "Type": "Object",
+        "Name": "Tree",
+        "ID": 4,
+        "HP": 5,
+        "Breakable": True,
+        "Collision_box": [0.85, 0.85, 0.35, 0.7],
+        "Obj_image": item_images.get("Tree"),
+        "Object_width": int(block_size * 2),
+        "Object_height": int(block_size * 2)
+    },
+    {
+        "Type": "Object",
+        "Name": "Stone",
+        "ID": 2,
+        "HP": 5,
+        "Breakable": True,
+        "Collision_box": [0.3, 0.25, 0.5, 0.4],
+        "Obj_image": item_images.get("Tree"),
+        "Object_width": int(block_size * 1),
+        "Object_height": int(block_size * 0.8)
+    },
+    {
+        "Type": "Object",
+        "Name": "Flower",
+        "ID": 5,
+        "Breakable": True,
+        "Obj_image": item_images.get("Tree"),
+        "Object_width": int(block_size * 0.5),
+        "Object_height": int(block_size * 0.5)    },
+    {
+        "Type": "Object",
+        "Name": "Mushroom",
+        "ID": 6,
+        "Breakable": True,
+        "Obj_image": item_images.get("Tree"),
+        "Object_width": int(block_size * 0.3),
+        "Object_height": int(block_size * 0.3)    },
+]
+
+# Testida asju mis on seotud ainult item'ga
+if __name__ == "__main__":
+    item_name_to_find = "Stone"
+    item_value_to_find = "Collision_box"
+
+    # Otsib listist itemi nime ja otsitavad valued
+    for item in items_list:
+        try:
+            if item["Name"].capitalize() == item_name_to_find.capitalize():
+                item_value = item[item_value_to_find]
+                print(f"The {item_value_to_find} of {item_name_to_find} is {item_value}")
+                break  # Väljub loopist peale itemi nime / value leidmist
+
+        except:
+            print(f"{item_name_to_find} has no {item_value_to_find}.")
+            break  # Väljub loopist kui itemi nime / valuet ei leitud
+
+    else:
+        # Kui itemi nime / value ei leitud listist
+        print(f"{item_name_to_find} not found in the list")
+
+    print()
+    print()
+    print()
+
+    # Tekitab tühja listi kuhu paneb itemid mille type on object
+    object_items = []
+
+    # Otsib listist itemi ID'd mille type on object
+    for item in items_list:
+        if item.get("Type") == "Object":
+            object_items.append(item)
+
+    # Otsib itemi ID / Collision_boxi ja prindib need välja ka siis kui listis pole seda olemas
+    for object_item in object_items:
+        print("ID:", object_item.get("ID", None))
+        print("HP:", object_item.get("HP", None))
+        print("Collision_box:", object_item.get("Collision_box", None))
+        print("test:", object_item.get("test", "Mind ei ole listis"))
+        print()
--- render.py
+++ render.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+from items import items_list
 import items
 from images import ground_images, water_images, item_images
 from objects import Object_Management # place_and_render_hitbox, place_and_render_object
@@ -56,60 +57,58 @@ class Render_Checker:
             # Teeb chunki render range laiuselt - test_list = [[1, 2, 3], [2, 3, 4], [3, 4, 5]]
             self.render_terrain_data.append(self.row)
 
-
     def object_list_creation(self) -> None:
-        """See func, object_list_creation, teeb ainult self.hit_boxes listi. Appendib sinna world objecktid."""
-
-        # Et ei tekiks lõpmatus arv pilte ühe ja sama objecti kohta
+        """Teeb objectidele hitboxid. Kasutab items.py items_list'i"""
         self.terrain_data_minerals: int = 0
         self.hit_boxes: list = []
         self.display_hit_box_decay: int = 0
 
+        # Teeb listi mis hoiab itemi ID'd ja Collision_box'i
+        object_collision_boxes = {}
+        # object_collision_boxes = {
+                                # 4: [0.85, 0.85, 0.35, 0.7],
+                                # 2: [0.3, 0.25, 0.5, 0.4],
+                                # 5: [0, 0, 0, 0],
+                                # 6: [0, 0, 0, 0]
+                                # }
+
+        # Lisab listi ID, collision_box'i
+        for item in items_list:
+            if item.get("Type") == "Object":
+                id = item.get("ID")
+                collision_box = item.get("Collision_box", [0, 0, 0, 0])
+                object_collision_boxes[id] = collision_box
 
         for row in self.render_terrain_data:
-            for j, i in row:
-                if 0 <= i < len(self.terrain_data) and 0 <= j < len(self.terrain_data[0]):
-                    # Kui terrain data [i][j] on suurem kui 1 siis arvutab
-                    # objecti asukoha ja hitboxi ning displayib pildi
-                    if self.terrain_data[i][j] in items.object_nr_list:
-                        terrain_x: int = j * self.block_size
-                        terrain_y: int = i * self.block_size
-                        object_id: int = self.terrain_data[i][j]
+            for x, y in row:
+                if 0 <= y < len(self.terrain_data) and 0 <= x < len(self.terrain_data[0]):
 
-                        if object_id in items.object_nr_list:
-                            self.terrain_data_minerals += 1
+                    # Vaatab kas itemi ID on dict'is:    object_collision_boxes = {}
+                    if self.terrain_data[y][x] in object_collision_boxes:
+                        terrain_x: int = x * self.block_size
+                        terrain_y: int = y * self.block_size
+                        object_id: int = self.terrain_data[y][x]
 
-                        # self.dimensions = [object_width, object_height, hit_box_width, hit_box_height, hit_box_offset_x, hit_box_offset_y]
-                        if object_id == 2:  # Rock
-                            self.dimensions: list[int, ...] = [1, 0.8, 0.5, 0.5, 0.3, 0.25]
+                        # Võtab õige itemi collision_box'i
+                        collision_box = object_collision_boxes.get(object_id, [0, 0, 0, 0])
 
+                        # Arvutab hitboxi suuruse ja asukoha vastavalt camera / player / render offsetile
+                        hit_box_offset_x_mlp, hit_box_offset_y_mlp, hit_box_width_mlp, hit_box_height_mlp = collision_box
+                        hit_box_width = int(self.block_size * hit_box_width_mlp)
+                        hit_box_height = int(self.block_size * hit_box_height_mlp)
 
-                        elif object_id == 4:  # Tree
-                            self.dimensions: list[int, ...] = [2, 2, 0.25, 0.65, 0.4, 0.2]
-
-                        elif object_id == 5:  # Flower
-                            self.dimensions: list[int, ...] = [0, 0, 0, 0, 0, 0]  # Neil ei pea ju hitboxe olema //
-                                                                                  #
-                                                                                  # // Ei pea ja aga neile peab andma
-                                                                                  # selle roosa kasti (interaction box vms)!!
-
-                        elif object_id == 6:  # Mushroom
-                            self.dimensions: list[int, ...] = [0, 0, 0, 0, 0, 0]
-
-                        object_width_mlp, object_height_mlp, hit_box_width_mlp, hit_box_height_mlp, hit_box_offset_x_mlp, hit_box_offset_y_mlp = self.dimensions
-                        object_width = int(self.block_size * object_width_mlp)
-                        object_height = int(self.block_size * object_height_mlp)
-                        hit_box_width = int(object_width * hit_box_width_mlp)
-                        hit_box_height = int(object_height * hit_box_height_mlp)
-
-                        hit_box_offset_x = int(object_width * hit_box_offset_x_mlp)
-                        hit_box_offset_y = int(object_height * hit_box_offset_y_mlp)
+                        hit_box_offset_x = int(self.block_size * hit_box_offset_x_mlp)
+                        hit_box_offset_y = int(self.block_size * hit_box_offset_y_mlp)
 
                         hit_box_x: int = terrain_x + hit_box_offset_x
                         hit_box_y: int = terrain_y + hit_box_offset_y
 
                         if self.display_hit_box_decay <= self.terrain_data_minerals:
-                            new_object: tuple[int, ...] = (hit_box_x, hit_box_y, hit_box_width, hit_box_height, object_id, hit_box_offset_x, hit_box_offset_y)
+                            new_object: tuple[int, ...] = (
+                            hit_box_x, hit_box_y, hit_box_width, hit_box_height, object_id, hit_box_offset_x,
+                            hit_box_offset_y)
 
-                            if new_object not in self.hit_boxes: self.hit_boxes.append(new_object)
+                            if new_object not in self.hit_boxes:
+                                self.hit_boxes.append(new_object)
+                                self.terrain_data_minerals += 1
                             self.display_hit_box_decay += 1
