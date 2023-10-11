4ce7f35c60da46f260f512c43edf429090feeaca
Oljake <olariviitak@gmail.com>
2023-10-11 22:38:25 +0300

    render when

--- items.py
+++ items.py
@@ -13,7 +13,7 @@ items_list = [
         "Collision_box": [0.85, 0.85, 0.35, 0.7],
         "Object_width": int(block_size * 2),
         "Object_height": int(block_size * 2),
-        "Render_when": block_size * 1
+        "Render_when": block_size * 0.8
         },
     {
         "Type": "Object",
@@ -24,7 +24,7 @@ items_list = [
         "Collision_box": [0.3, 0.25, 0.5, 0.4],
         "Object_width": int(block_size * 1),
         "Object_height": int(block_size * 0.8),
-        "Render_when": -(block_size * 1)
+        "Render_when": -(block_size * 0.1)
         },
     {
         "Type": "Object",
@@ -33,7 +33,7 @@ items_list = [
         "Breakable": True,
         "Object_width": int(block_size * 0.5),
         "Object_height": int(block_size * 0.5),
-        "Render_when": -(block_size * 0.3)
+        "Render_when": -(block_size * 0.23)
         },
     {
         "Type": "Object",
@@ -42,7 +42,7 @@ items_list = [
         "Breakable": True,
         "Object_width": int(block_size * 0.3),
         "Object_height": int(block_size * 0.3),
-        "Render_when": -(block_size * 1)
+        "Render_when": -(block_size * 0.45)
         },
 ]
 
