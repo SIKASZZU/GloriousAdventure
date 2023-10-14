21e54115175286f647ed71d305711de3c6aa0b65
SIKASZZU <soots.sigvrid@gmail.com>
2023-10-14 16:57:03 +0300

    crafting_menu jne

--- crafting.py
+++ crafting.py
@@ -1,4 +1,17 @@
-import pygame
+class Crafting:
+    def crafting(self):
+        pass
+        # kammib terve invi l2bi
+        # vaatba kas seal on 4 puitu
 
-def crafting(self):
-    pass
+    def crafting_menu(self):
+        
+        crafting_menu_items = {
+            "Workbench": {
+                "Wood": 4
+            },
+            "Pickaxe": {
+                "Stick": 2,
+                "Stone": 3
+            }
+        }
