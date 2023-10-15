ad794ee9eb2a44385cea8abcb0a4bf40231dcbc6
SIKASZZU <soots.sigvrid@gmail.com>
2023-10-15 19:51:02 +0300

    refactor, classidesse oma variablid

--- stamina.py
+++ stamina.py
@@ -1,6 +1,6 @@
 import pygame
 import time
-
+from HUD import HUD_class
 
 class StaminaComponent:
     """
@@ -53,26 +53,26 @@ class StaminaComponent:
             self.max_stamina = 20
 
     def get_stamina(self):
-        return self.current_stamina
+      return self.current_stamina
 
     def stamina_bar_update(self):
+      stamina_rect, stamina_bar_border, stamina_bar_bg = HUD_class.stamina_bar(self, HUD_class.half_w)
 
-        # Functiooni algus
-        if self.stamina_bar_decay == 120:
-            self.stamina_rect_bg = pygame.Rect(0, 0, 0, 0)
-            self.stamina_rect = pygame.Rect(0, 0, 0, 0)
-            self.stamina_rect_border = pygame.Rect(0, 0, 0, 0)
+      if self.stamina_bar_decay == 120:
+        stamina_bar_bg = pygame.Rect(0, 0, 0, 0)
+        stamina_rect = pygame.Rect(0, 0, 0, 0)
+        stamina_bar_border = pygame.Rect(0, 0, 0, 0)
 
-        if self.player.stamina.current_stamina >= self.player.stamina.max_stamina:
-            self.stamina_bar_decay += 1
+      if self.player.stamina.current_stamina >= self.player.stamina.max_stamina:
+          self.stamina_bar_decay += 1
 
-        else:
-            self.stamina_bar_size = self.player.stamina.current_stamina * self.ratio  # arvutab stamina bari laiuse
-            self.stamina_rect_bg = pygame.Rect(self.half_w - (self.stamina_bar_size_bg / 2) - 6, self.screen_y - 75,
-                                              self.stamina_bar_size_bg + 12, 15)  # Kui staminat kulub, ss on background taga
-            
-            self.stamina_rect_border = pygame.Rect(self.half_w - (self.stamina_bar_size_border / 2) - 6, self.screen_y - 75, 
-                                                  self.stamina_bar_size_border + 12, 15)  # K6igi stamina baride ymber border
-            
-            self.stamina_rect = pygame.Rect(self.half_w - (self.stamina_bar_size / 2) - 6, self.screen_y - 75,
-                                            self.stamina_bar_size + 12, 15)
+      else:
+          HUD_class.stamina_bar_size = self.player.stamina.current_stamina * HUD_class.ratio  # arvutab stamina bari laiuse
+          stamina_bar_bg = pygame.Rect(HUD_class.half_w - (HUD_class.stamina_bar_size_bg / 2) - 6, self.screen_y - 75,
+                                            HUD_class.stamina_bar_size_bg + 12, 15)  # Kui staminat kulub, ss on background taga
+          
+          stamina_bar_border = pygame.Rect(HUD_class.half_w - (HUD_class.stamina_bar_size_border / 2) - 6, self.screen_y - 75, 
+                                                HUD_class.stamina_bar_size_border + 12, 15)  # K6igi stamina baride ymber border
+          
+          stamina_rect = pygame.Rect(HUD_class.half_w - (HUD_class.stamina_bar_size / 2) - 6, self.screen_y - 75,
+                                          HUD_class.stamina_bar_size + 12, 15)
