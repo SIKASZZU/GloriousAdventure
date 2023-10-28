6b79d82ad145831cbe43dabe5b8ea825eab7cdf0
SIKASZZU <soots.sigvrid@gmail.com>
2023-10-28 12:26:48 +0300

    fixed multiple inputs

--- menu.py
+++ menu.py
@@ -3,12 +3,42 @@ import sys
 
 from variables import UniversalVariables
 from images import menu_images
-from button import Button
+
+# button class
+class Button():
+    def __init__(self, x, y, image, scale):
+        width = image.get_width()
+        height = image.get_height()
+        self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+    
+    clicked = False  # Kui see on self.clicked, siis tekib mitu inputi
+
+    def draw(self, surface):
+        action = False
+        # get mouse position
+        pos = pygame.mouse.get_pos()
+
+        # check mouseover and clicked conditions
+        if self.rect.collidepoint(pos):
+            #print(Button.clicked, "Button.clicked")
+            if pygame.mouse.get_pressed()[0] == 1 and Button.clicked == False:
+                Button.clicked = True
+                action = True
+
+        if pygame.mouse.get_pressed()[0] == 0:
+            Button.clicked = False
+
+        # draw button on screen
+        surface.blit(self.image, (self.rect.x, self.rect.y))
+
+        return action
+
 
 class Menu():
 
     screen_x = UniversalVariables.screen_x
-
     resume_button = Button(screen_x / 2 - 100, 175, menu_images["resume_img"], 1)
     options_button = Button(screen_x / 2 - 106, 300, menu_images["options_img"], 1)
     video_button = Button(screen_x / 2 - 178, 125, menu_images["video_img"], 1)
