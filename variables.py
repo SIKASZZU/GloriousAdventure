8ce02a4cdd02fdd04aa4a7e2bc5f397c47472929
SIKASZZU <soots.sigvrid@gmail.com>
2023-10-29 19:12:41 +0200

    removed vision system

--- variables.py
+++ variables.py
@@ -26,10 +26,3 @@ class UniversalVariables:
     # ******************** OFFSET ******************** #
     offset_x: int = 0
     offset_y: int = 0
-
-    # ******************** VISION- / LIGHTSYSTEM ******************** # 
-    TILE_SIZE = block_size
-    ZERO_TOLERANCE = 1e-10
-    FILL_COLOR = (69,69,69)
-    BORDER_BLOCK = 5
-    WWINDOW_SIZE = (len(full_map_data[0])*TILE_SIZE, len(full_map_data)*TILE_SIZE)
