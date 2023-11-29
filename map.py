2cc0bbd3218fd57b3754b93ef8ebefbacd6b103b
SIKASZZU <soots.sigvrid@gmail.com>
2023-11-29 19:35:57 +0200

    sama kood, vähe rohkem vinks vonks

--- map.py
+++ map.py
@@ -3,7 +3,7 @@ import numpy as np
 class MapData:
     width = 40
     height = 40
-    maze_count = 4
+    maze_count = 2
 
     map_data = []
     maze_data = []
@@ -31,9 +31,9 @@ class MapData:
         
         ### TODO: Kui tahta muuta maze kuju, siis muuta maze_data. Hetkel genereerib lihtsalt random ruute
         ### TODO: Mazei põrand oleks stone, mitte terrain.
-        maze_data_np_array = np.random.choice([99, 98], size=(width, height), p=[0.3, 0.7])
+        maze_data_np_array = np.random.choice([99, 98], size=(width, height), p=[0.35, 0.65])
         maze_data = maze_data_np_array.tolist()
-        return maze_data
+        return maze_data  # type = list
     
     def map_creation():
         map_data = MapData.map_data
@@ -41,27 +41,31 @@ class MapData:
         maze_data = MapData.maze_creation()
         glade_data = MapData.glade_creation()
 
+        if not map_data:  # list is empty
+            map_data = glade_data
+        else: pass
+
         # Lisab glade_data ja maze_data kokku ning paneb selle teatud kohta
+        print(f'process: {maze_count},\n {map_data} \n\n {maze_data}')
         
         if MapData.maze_count == 0:  # add new maze to: top
-            map_data = maze_data + glade_data
-            print(f'process:\n {maze_count},\n {map_data} \n=\n {maze_data} \n+\n {glade_data}')
+            new_map_data = maze_data + map_data
 
         if MapData.maze_count == 1:  # add new maze to: bottom
-            map_data = glade_data + maze_data
-            print(f'process:\n {maze_count},\n {map_data} \n=\n {maze_data} \n+\n {glade_data}')
+            new_map_data = map_data + maze_data
 
-        if MapData.maze_count == 3:  # add new maze to: left
-            map_data = [new_row + original_row for new_row, original_row in zip(maze_data, glade_data)]
-            print(f'process:\n {maze_count},\n {map_data} \n=\n {maze_data} \n+\n {glade_data}')
+        if MapData.maze_count == 2:  # add new maze to: left
+            new_map_data = [new_row + original_row for new_row, original_row in zip(maze_data, map_data)]
 
-        if MapData.maze_count == 4:  # add new maze to: right
-            map_data = [original_row + new_row for original_row, new_row in zip(glade_data, maze_data)]
-            print(f'process:\n {maze_count},\n {map_data} \n=\n {maze_data} \n+\n {glade_data}')
+        if MapData.maze_count == 3:  # add new maze to: right
+            new_map_data = [original_row + new_row for original_row, new_row in zip(glade_data, map_data)]
+        map_data = new_map_data
+        print(f'\nprocess: {maze_count}, mapdata RESULT: {map_data}')
 
         ### TODO: Error props, kui map_data list on juba maze_data + glade_data. 
             # Error tuleneb sellest, et need listid pole sama dimensioonidega. Yks on suurem kui teine.
 
+        ### TODO: K6igile mazecountidel on erinev, kuhu sein peaks tekkima.
         # Muudab maze 22red seinteks.
         #map_data[0, :] = 99  # top
         #map_data[:, -1] = 99  # parem kylg
@@ -72,4 +76,4 @@ class MapData:
         return MapData.map_data
 
 if __name__ == "__main__":
-    print('lol')
+    MapData.map_creation()
