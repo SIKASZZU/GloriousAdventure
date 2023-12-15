ac112d0eab4caff04b6495595dd9917e03aa2b58
Oljake <olariviitak@gmail.com>
2023-12-15 17:26:47 +0200

    Decorators...

--- test.py
+++ test.py
@@ -1,49 +1,30 @@
-import numpy as np
+# defining a decorator
+def hello_decorator(func):
+    # inner1 is a Wrapper function in
+    # which the argument is called
 
-class MapData:
-    width = 40
-    height = 40
-    map_data = []
+    # inner function can access the outer local
+    # functions like in this case "func"
+    def inner1():
+        print("Hello, this is before function execution")
 
-    # Create Glade
-    @staticmethod
-    def create_glade():
-        glade_data = np.full((10, 10), 99)  # Example smaller glade (Modify as needed)
-        return glade_data
+        # calling the actual function now
+        # inside the wrapper function.
+        func()
 
-    # Create Maze
-    @staticmethod
-    def create_maze():
-        maze_data = np.full((20, 20), 98)  # Example larger maze (Modify as needed)
-        return maze_data
+        print("This is after function execution")
 
-    # Create Boss
-    @staticmethod
-    def create_boss():
-        boss_data = np.full((8, 8), 97)  # Example smaller boss area (Modify as needed)
-        return boss_data
-
-    @staticmethod
-    def map_creation():
-        glade_data = MapData.create_glade()
-        maze_data = MapData.create_maze()
-        boss_data = MapData.create_boss()
-
-        # Initialize map with default value 100 (empty space)
-        MapData.map_data = np.full((MapData.width, MapData.height), 100)
-
-        # Place Glade in the map (Modify as per your requirement)
-        MapData.map_data[:glade_data.shape[0], :glade_data.shape[1]] = glade_data
-
-        # Place Maze in the map (Modify as per your requirement)
-        MapData.map_data[5:25, 5:25] = maze_data[:20, :20]
-
-        # Place Boss in the map (Modify as per your requirement)
-        MapData.map_data[10:18, 10:18] = boss_data
-
-        return MapData.map_data
+    return inner1
 
 
-if __name__ == "__main__":
-    map = MapData.map_creation()
-    print(np.array(map))  # Print the generated map
+# defining a function, to be called inside wrapper
+def function_to_be_used():
+    print("This is inside the function !!")
+
+
+# passing 'function_to_be_used' inside the
+# decorator to control its behaviour
+function_to_be_used = hello_decorator(function_to_be_used)
+
+# calling the function
+function_to_be_used()
