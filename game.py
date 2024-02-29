f4b09f27acc14b1e2970abb7478c9f1de7f8702d
SIKASZZU <soots.sigvrid@gmail.com>
2024-02-29 20:40:10 +0200

    player zoom fix, zoomib playerit ka nyyd

--- game.py
+++ game.py
@@ -95,14 +95,22 @@ class Game:
                         if event.button == 4:  # Scroll +
                             UniversalVariables.block_size += 1  # Increase block_size
 
+                            UniversalVariables.player_height: int = UniversalVariables.block_size * 0.65
+                            UniversalVariables.player_width: int = UniversalVariables.block_size * 0.65
+
+                            UniversalVariables.player_hitbox_offset_x = 0.29 * UniversalVariables.player_height
+                            UniversalVariables.player_hitbox_offset_y = 0.22 * UniversalVariables.player_width
+
                         elif event.button == 5:  # Scroll -
                             UniversalVariables.block_size -= 1  # Decrease block_size
                             if UniversalVariables.block_size < 1:  # Prevent block_size from being less than 1
                                 UniversalVariables.block_size = 1
 
-                        # Võtab clicki positsiooni
-                        elif event.type == pygame.MOUSEBUTTONDOWN:
-                            self.click_position = event.pos
+                            UniversalVariables.player_height: int = UniversalVariables.block_size * 0.65
+                            UniversalVariables.player_width: int = UniversalVariables.block_size * 0.65
+
+                            UniversalVariables.player_hitbox_offset_x = 0.29 * UniversalVariables.player_height
+                            UniversalVariables.player_hitbox_offset_y = 0.22 * UniversalVariables.player_width
 
             # Vaatab kas mäng on tööle pandud või mitte
             if Menu.game_state:
