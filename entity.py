4be445ae54eba1cfab3d30f9c65b3d663de07a48
Oljake <109909586+Oljake@users.noreply.github.com>
2024-03-12 11:51:37 +0200

    Create entity.py

---
+++ entity.py
@@ -0,0 +1,19 @@
+import pygame
+
+class Entity:
+  def __init__(self):
+
+    # health
+    # speed
+    # attack
+    
+    pass
+
+class Mob:
+  def __init__(self):
+
+    super(Entity(__init_))
+
+    pass
+    
+  
