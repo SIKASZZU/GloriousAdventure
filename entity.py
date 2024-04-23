0ba2284fd442d056d5d87407244930f6aa75a19a
SIKASZZU <soots.sigvrid@gmail.com>
2024-04-23 21:46:20 +0300

    progress

--- entity.py
+++ entity.py
@@ -53,14 +53,26 @@ class Enemy:
     def despawn(self):
         # {'Enemy_0': (26, 21),
         if EssentsialsUpdate.day_night_text == 'Day':
-            Enemy.spawned_enemy_dict: dict[str, tuple[pygame.Surface, int, int]] = {}  # (Enemy_image, y, x)
+            Enemy.spawned_enemy_dict: dict[str, tuple[pygame.Surface, int, int]] = {}  # resetting list of enemies (Enemy_image, y, x)
             UniversalVariables.enemy_spawnpoint_list = set()
             UniversalVariables.enemy_counter = 0
 
     def move(self):
         """ Liikumine paremale, vasakule, yles, alla. Liigub playeri suunas, kui detected. Liigub ainult object idl 98. """
 
-        # Function to calculate speed based on movement
+        def calculate_next_step(current_position, next_position):
+            if next_position == 'right': next_step = (1, 0)  # +1
+            if next_position == 'left': next_step = (-1, 0)  # -1
+            if next_position == 'top': next_step = (0, -1) 
+            if next_position == 'down': next_step = (0, 1)
+
+
+            ''' next position on suund, mitte koordinaat. left, right, up, down. '''
+            for enemy in Enemy.spawned_enemy_dict.values():
+                if enemy[1][2] == current_position:
+                    enemy[1][2] 
+
+        # funk, et arvutada kiirendus kummitusele.
         def calculate_speed(x1, y1, x2, y2, default_speed, block_size):
             if (x2 - x1) != 0 and (y2 - y1) != 0:  # If movement is along both axes
                 return default_speed
@@ -71,29 +83,47 @@ class Enemy:
                 # Speed increases by the total number of blocks moved in both axes
                 return default_speed + blocks_moved_x + blocks_moved_y
 
+        
+        # detectioni peale hakkab liikuma
+        if self.direction != 'none': 
+            for enemy in Enemy.spawned_enemy_dict.values():
+                current_position = (enemy[1], enemy[2])
+                calculate_next_step(current_position, self.direction)
+        
+
+
+        # hakkab liikuma oma rutiinset sitta, yles alla paremale vasakule
         block_size = UniversalVariables.block_size
         x = abs(x)
-
         if x - block_size > x < x + block_size:
             # do that
             ...
 
-    def detection(self, player_x, player_y):
+
+    def detection(self):
         block_size = UniversalVariables.block_size
+        player_x = UniversalVariables.player_x
+        player_y = UniversalVariables.player_y
+
         for enemy_name, enemy_info in Enemy.spawned_enemy_dict.items():
             enemy_x, enemy_y = enemy_info[1], enemy_info[2]
-            distance_to_player_x_grid = abs((player_x // block_size) - (enemy_x // block_size))
-            distance_to_player_y_grid = abs((player_y // block_size) - (enemy_y // block_size))
 
-            print(distance_to_player_x_grid, " <= ", 10)
-            print(distance_to_player_y_grid, " <= ", 10)
+            player_x_grid = int(player_x // block_size)
+            player_y_grid = int(player_y // block_size)
+            
+            dx = player_x_grid - enemy_x
+            dy = player_y_grid - enemy_y
+            
+            distance_to_player_x_grid = abs(player_x_grid - enemy_x)
+            distance_to_player_y_grid = abs(player_y_grid - enemy_y)
 
-            if distance_to_player_x_grid <= 10 or distance_to_player_y_grid <= 10:
-                dx = player_x - enemy_x
-                dy = player_y - enemy_y
+            if abs(distance_to_player_x_grid) <= 10 and abs(distance_to_player_y_grid) <= 10:
 
                 # Update direction
-                if abs(dx) > abs(dy):
+                if abs(dx) == abs(dy):
+                    print('ontop')   # vb peab olema pass, player dead
+
+                elif abs(dx) > abs(dy):
                     if dx > 0:
                         self.direction = 'right'
                         print('right')
@@ -115,5 +145,5 @@ class Enemy:
 
     def update(self):
         Enemy.spawn(self)
-        Enemy.detection(self, UniversalVariables.player_x, UniversalVariables.player_y)
+        Enemy.detection(self)
         Enemy.despawn(self)
