091a32c629c27dfdfa8479e92731345b566be05d
Oljake <olariviitak@gmail.com>
2024-04-24 21:10:26 +0300

    Ghost collision

--- entity.py
+++ entity.py
@@ -1,3 +1,4 @@
+import camera
 import random
 
 import pygame
@@ -6,10 +7,9 @@ from variables import UniversalVariables
 from update import EssentsialsUpdate
 from images import ImageLoader
 
-ghost_image = pygame.transform.scale(ImageLoader.load_sprite_image("Ghost"), (UniversalVariables.block_size, UniversalVariables.block_size))
-
 
 class Enemy:
+    ghost_image = pygame.transform.scale(ImageLoader.load_sprite_image("Ghost"), (UniversalVariables.block_size // 1.5, UniversalVariables.block_size // 1.5))
     spawned_enemy_dict: dict[str, tuple[pygame.Surface, int, int]] = {}  # (Enemy_image, y, x)
     enemy_in_range: set[tuple[str, str]] = set()
 
@@ -34,15 +34,16 @@ class Enemy:
                         abs(player_grid[1] - spawn_point[1]) > distance_from_player):
 
                     # Spawn an enemy
-                    Enemy.spawned_enemy_dict[f'Enemy_{UniversalVariables.enemy_counter}'] = ghost_image, spawn_point[1], \
-                    spawn_point[0]
+                    Enemy.spawned_enemy_dict[f'Enemy_{UniversalVariables.enemy_counter}'] = Enemy.ghost_image, \
+                    spawn_point[1], \
+                        spawn_point[0]
 
                     spawned_enemy_count += 1
                     UniversalVariables.enemy_counter += 1
 
                     # Limiteerib Enemite arvu vastvalt maze_counterile
                     max_enenmy = 5 * UniversalVariables.maze_counter
-                    if UniversalVariables.maze_counter == 1: max_enenmy = 2  # Kui on ainult 1 maze siis spawnib max 2 enemit
+                    if UniversalVariables.maze_counter == 1: max_enenmy = 1  # Kui on ainult 1 maze siis spawnib max 2 enemit
                     if spawned_enemy_count >= max_enenmy:
                         break
 
@@ -50,132 +51,121 @@ class Enemy:
             enemy_x = enemy[1] * UniversalVariables.block_size + UniversalVariables.offset_x
             enemy_y = enemy[2] * UniversalVariables.block_size + UniversalVariables.offset_y
 
-            UniversalVariables.screen.blit(pygame.transform.scale(enemy[0], (UniversalVariables.block_size, UniversalVariables.block_size)), (enemy_x, enemy_y))
+            UniversalVariables.screen.blit(enemy[0], (enemy_x, enemy_y))
 
     @staticmethod
     def despawn():
+        """  Despawnib kõik enemyd, kui on päev. """
+
         if EssentsialsUpdate.day_night_text == 'Day':
             Enemy.spawned_enemy_dict = {}  # resetting list of enemies (Enemy_name, Enemy_image, x, y)
             UniversalVariables.enemy_spawnpoint_list = set()
             UniversalVariables.enemy_counter = 0
 
     def move(self):
-        """ Liikumine paremale, vasakule, yles, alla. Liigub playeri suunas, kui detected. Liigub ainult object idl 98. """
+        """Move enemies based on their individual decisions."""
 
         for enemy_name, enemy_info in Enemy.spawned_enemy_dict.items():
             image, x, y = enemy_info
-            for enemy_name_, direction in Enemy.enemy_in_range:
+            direction = None
 
+            for enemy_name_, dir_ in Enemy.enemy_in_range:
                 if enemy_name == enemy_name_:
-                    print(direction)
-                    print(enemy_info)
+                    direction = dir_
+                    break
 
-                    if direction == 'right':
-                        Enemy.spawned_enemy_dict[enemy_name] = image, x + 0.03, y
+            # If the enemy is in range of the player, move accordingly
+            if direction:
+                # Calculate the next position based on direction
+                next_x, next_y = x, y
+                if direction == 'right':
+                    next_x += 0.03
+                elif direction == 'left':
+                    next_x -= 0.03
+                elif direction == 'down':
+                    next_y += 0.03
+                elif direction == 'up':
+                    next_y -= 0.03
 
-                    if direction == 'left':
-                        Enemy.spawned_enemy_dict[enemy_name] = image, x - 0.03, y
+            else:
+                # If the enemy is not in range, move randomly
+                direction = random.choice(['right', 'left', 'down', 'up'])
+                next_x, next_y = x, y
+                if direction == 'right':
+                    next_x += 0.03
+                elif direction == 'left':
+                    next_x -= 0.03
+                elif direction == 'down':
+                    next_y += 0.03
+                elif direction == 'up':
+                    next_y -= 0.03
 
-                    if direction == 'down':
-                        Enemy.spawned_enemy_dict[enemy_name] = image, x, y + 0.03
+            # enemy_info[0]  # image
+            # self.terrain_data[next_x][next_yx]
+            enemy_corner_set: set[tuple[float, float], ...] = set()
+            enemy_width = (enemy_info[0].get_width())
+            enemy_height = enemy_info[0].get_height()
+            print(enemy_info[0])
 
-                    if direction == 'up':
-                        Enemy.spawned_enemy_dict[enemy_name] = image, x, y - 0.03
+            block_size = UniversalVariables.block_size
 
+            enemy_corner_set.add((x * block_size, y * block_size))                                  # Top-Left
+            enemy_corner_set.add((x * block_size + enemy_width, y * block_size))                    # Top-Right
+            enemy_corner_set.add((x * block_size, y * block_size + enemy_height))                   # Bottom-Left
+            enemy_corner_set.add((x * block_size + enemy_width, y * block_size + enemy_height))     # Bottom-Right
+
+
+            for next_cords in enemy_corner_set:
+                new_next_x, new_next_y = int(next_cords[0] // block_size), int(next_cords[1] // block_size)
+
+                if self.terrain_data[new_next_y][new_next_x] != 99 and self.terrain_data[new_next_y][new_next_x] != 933 and self.terrain_data[new_next_y][new_next_x] != 977:
+                    x, y = next_x, next_y
 
                 else:
-                    direction = random.choice(['right', 'left', 'down', 'up'])
+                    print("get cancer", self.terrain_data[new_next_y][new_next_x])
+                    break
 
-                    if direction == 'right':
-                        Enemy.spawned_enemy_dict[enemy_name] = image, x + 0.03, y
-
-                    if direction == 'left':
-                        Enemy.spawned_enemy_dict[enemy_name] = image, x - 0.03, y
-
-                    if direction == 'down':
-                        Enemy.spawned_enemy_dict[enemy_name] = image, x, y + 0.03
-
-                    if direction == 'up':
-                        Enemy.spawned_enemy_dict[enemy_name] = image, x, y - 0.03
-
-
-
-        # def calculate_next_step(current_position, next_position):
-        #     if next_position == 'right': next_step = (1, 0)  # +1
-        #     if next_position == 'left': next_step = (-1, 0)  # -1
-        #     if next_position == 'top': next_step = (0, -1)
-        #     if next_position == 'down': next_step = (0, 1)
-        #
-        #
-        #     ''' next position on suund, mitte koordinaat. left, right, up, down. '''
-        #     for enemy in Enemy.spawned_enemy_dict.values():
-        #         if enemy[1][2] == current_position:
-        #             enemy[1][2]
-        #
-        # # funk, et arvutada kiirendus kummitusele.
-        # def calculate_speed(x1, y1, x2, y2, default_speed, block_size):
-        #     if (x2 - x1) != 0 and (y2 - y1) != 0:  # If movement is along both axes
-        #         return default_speed
-        #     else:
-        #         # Calculate number of blocks moved in each axis
-        #         blocks_moved_x = abs(x2 - x1) / block_size
-        #         blocks_moved_y = abs(y2 - y1) / block_size
-        #         # Speed increases by the total number of blocks moved in both axes
-        #         return default_speed + blocks_moved_x + blocks_moved_y
-
-        
-        #   # detectioni peale hakkab liikuma
-        #   if direction != 'none':
-        #       for enemy in Enemy.spawned_enemy_dict.values():
-        #           current_position = (enemy[1], enemy[2])
-        #           calculate_next_step(current_position, direction)
-        
-
-        # else:
-        #     pass
-        #     # hakkab liikuma oma rutiinset sitta, yles alla paremale vasakule
-
+            # Update the enemy's position
+            Enemy.spawned_enemy_dict[enemy_name] = image, x, y
 
     def detection(self):
-        block_size = UniversalVariables.block_size
-        player_x = UniversalVariables.player_x
-        player_y = UniversalVariables.player_y
+        player_window_x = camera.Camera.player_window_x
+        player_window_y = camera.Camera.player_window_y
 
         Enemy.enemy_in_range = set()
 
         for enemy_name, enemy_info in Enemy.spawned_enemy_dict.items():
-            enemy_x, enemy_y = enemy_info[1], enemy_info[2]
+            enemy_x_grid, enemy_y_grid = enemy_info[1], enemy_info[2]
 
-            player_x_grid = int(player_x // block_size)
-            player_y_grid = int(player_y // block_size)
-            
-            dx = player_x_grid - enemy_x
-            dy = player_y_grid - enemy_y
-            
-            distance_to_player_x_grid = abs(player_x_grid - enemy_x)
-            distance_to_player_y_grid = abs(player_y_grid - enemy_y)
+            enemy_x = enemy_x_grid * UniversalVariables.block_size + UniversalVariables.offset_x
+            enemy_y = enemy_y_grid * UniversalVariables.block_size + UniversalVariables.offset_y
 
-            if abs(distance_to_player_x_grid) <= 10 and abs(distance_to_player_y_grid) <= 10:
+            distance_to_player_x_grid = player_window_x - enemy_x
+            distance_to_player_y_grid = player_window_y - enemy_y
+
+            print('distance_to_player_x_grid', distance_to_player_x_grid, 'distance_to_player_y_grid',
+                  distance_to_player_y_grid)
+
+            if abs(distance_to_player_x_grid) <= 1000 and abs(distance_to_player_y_grid) <= 1000:
                 direction: str = 'none'
 
                 # Update direction
-                if distance_to_player_x_grid < 0.5 and distance_to_player_y_grid < 0.5:
-                    print('ontop')   # vb peab olema pass, player dead
+                if abs(distance_to_player_x_grid) < 15 and abs(distance_to_player_y_grid) < 15:
+                    print('ontop')  # vb peab olema pass, player dead
 
-                elif abs(dx) > abs(dy):
-                    if dx > 0:
+                elif abs(distance_to_player_x_grid) > abs(distance_to_player_y_grid):
+                    if distance_to_player_x_grid > 0:
                         direction = 'right'
                     else:
                         direction = 'left'
 
                 else:
-                    if dy > 0:
+                    if distance_to_player_y_grid > 0:
                         direction = 'down'
                     else:
                         direction = 'up'
 
                 Enemy.enemy_in_range.add((enemy_name, direction))
-
 
     def attack(self):
         ...
