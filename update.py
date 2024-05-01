3c9d2bf9f5ec449b0985255289b01d936e57538c
SIKASZZU <soots.sigvrid@gmail.com>
2024-05-01 12:24:14 +0300

    removed print statement

--- update.py
+++ update.py
@@ -185,7 +185,6 @@ class EssentsialsUpdate:
         # Update day, night text next to game_day_count
         
         if hours < 8 or hours >= 22: 
-            print('night')
             day_night_text = 'Night'
         else: 
             day_night_text = 'Day'
