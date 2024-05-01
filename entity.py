64bdf07cc65a7acdd7ae15acba0024040c46faa2
SIKASZZU <soots.sigvrid@gmail.com>
2024-05-01 14:02:50 +0300

    added TODOs, pathfinding works too fast, progress

--- entity.py
+++ entity.py
@@ -8,6 +8,11 @@ from update import EssentsialsUpdate
 from images import ImageLoader
 
 
+### TODO:
+    # ghost collision
+    # kui ghost on samal gridil mis player, v6i selle k6rval, ss hakkab koordinaatidega arvutama.
+
+
 class Enemy:
     ghost_image = pygame.transform.scale(ImageLoader.load_sprite_image("Ghost"), (UniversalVariables.block_size // 1.5, UniversalVariables.block_size // 1.5))
     spawned_enemy_dict: dict[str, tuple[pygame.Surface, int, int]] = {}  # (Enemy_image, y, x)
@@ -76,6 +81,39 @@ class Enemy:
 
             Enemy.enemy_in_range.clear()
 
+
+    # TODO: pathfinding eraldi faili viia
+
+    def is_valid(self, x, y):
+        """ Check if coordinates (x, y) are valid in the maze. """
+
+        return 0 <= x < len(self.terrain_data) and 0 <= y < len(self.terrain_data[x]) and self.terrain_data[x][y] != 99
+
+    def find_path_bfs(self, start, end):
+        """ Breadth-First Search algorithm to find a path from start to end in the maze. """
+        from collections import deque
+
+        queue = deque([(start, [])])
+        print(queue)
+        visited = set()
+        directions = [(0, -1), (0, 1), (-1, 0), (1, 0)]
+
+
+        while queue:
+            (x, y), path = queue.popleft()
+            if (x, y) == end:
+                return path
+
+            if (x, y) not in visited:
+                visited.add((x, y))
+                for dx, dy in directions:
+                    new_x, new_y = x + dx, y + dy
+                    if Enemy.is_valid(self, new_x, new_y):
+                        new_path = path + [(new_x, new_y)]
+                        queue.append(((new_x, new_y), new_path))
+
+        return None
+
     def move(self):
         """ Move enemies based on their individual decisions."""
 
@@ -101,8 +139,32 @@ class Enemy:
                 elif direction == 'up':
                     next_y -= 0.03
 
+                # If the enemy is not in range, move using pathfinding
+                player_grid = (int(UniversalVariables.player_y // UniversalVariables.block_size), int(UniversalVariables.player_x // UniversalVariables.block_size))
+                enemy_grid = (enemy_info[2], enemy_info[1])
+
+                player_grid = (int(player_grid[0]), int(player_grid[1]))
+                enemy_grid = (int(enemy_grid[0]), int(enemy_grid[1]))
+
+                # Perform pathfinding
+                path = Enemy.find_path_bfs(self, enemy_grid, player_grid)
+                print('path', path)
+                print('player grid', player_grid)
+                print((int(enemy_grid[0]), int(enemy_grid[1])))
+
+                if path:
+                    # Move towards the next position in the path
+                    next_node = path[0]  # Assuming the first node is the current position
+
+                    # next_x = next_node[1] * UniversalVariables.block_size + UniversalVariables.offset_x - 250
+                    # next_y = next_node[0] * UniversalVariables.block_size + UniversalVariables.offset_y - 250
+                    print('entity next move:', next_x, next_y)
+                    x = next_node[1]
+                    y = next_node[0]
+
+
             else:
-                # If the enemy is not in range, move randomly
+                # If no path found, move randomly
                 direction = random.choice(['right', 'left', 'down', 'up'])
                 next_x, next_y = x, y
                 if direction == 'right':
@@ -113,6 +175,7 @@ class Enemy:
                     next_y += 0.03
                 elif direction == 'up':
                     next_y -= 0.03
+                x, y = next_x, next_y
 
 
             enemy_corner_set: set[tuple[float, float], ...] = set()
@@ -122,23 +185,23 @@ class Enemy:
             block_size = UniversalVariables.block_size
 
             # Testib cornerid 2ra tuleviku koordinaatidega.
-            enemy_corner_set.add((next_x * block_size, next_y * block_size))                                  # Top-Left
-            enemy_corner_set.add((next_x * block_size + enemy_width, next_y * block_size))                    # Top-Right
-            enemy_corner_set.add((next_x * block_size, next_y * block_size + enemy_height))                   # Bottom-Left
-            enemy_corner_set.add((next_x * block_size + enemy_width, next_y * block_size + enemy_height))     # Bottom-Right
-
-            for next_cords in enemy_corner_set:
-                new_grid_x, new_grid_y = int(next_cords[0] // block_size), int(next_cords[1] // block_size)
-
-                if self.terrain_data[new_grid_y][new_grid_x] != 99 and \
-                    self.terrain_data[new_grid_y][new_grid_x] != 933 and \
-                    self.terrain_data[new_grid_y][new_grid_x] != 977:
-                    continue
-                else:
-                    break
-            else:
-                x, y = next_x, next_y
-
+            # enemy_corner_set.add((next_x * block_size, next_y * block_size))                                  # Top-Left
+            # enemy_corner_set.add((next_x * block_size + enemy_width, next_y * block_size))                    # Top-Right
+            # enemy_corner_set.add((next_x * block_size, next_y * block_size + enemy_height))                   # Bottom-Left
+            # enemy_corner_set.add((next_x * block_size + enemy_width, next_y * block_size + enemy_height))     # Bottom-Right
+            
+            # for next_cords in enemy_corner_set:
+            #     new_grid_x, new_grid_y = int(next_cords[0] // block_size), int(next_cords[1] // block_size)
+            #     if self.terrain_data[new_grid_y][new_grid_x] != 99 and \
+            #         self.terrain_data[new_grid_y][new_grid_x] != 933 and \
+            #         self.terrain_data[new_grid_y][new_grid_x] != 977:
+            #         continue
+            #     else:
+            #         break
+            # else:
+            #     x, y = next_x, next_y
+            print(Enemy.spawned_enemy_dict[enemy_name])
+            print(Enemy.spawned_enemy_dict)
             Enemy.spawned_enemy_dict[enemy_name] = image, x, y
 
 
