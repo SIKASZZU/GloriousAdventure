4e7480719e87fb6795115870bd29ff04a3f1e8e5
SIKASZZU <soots.sigvrid@gmail.com>
2024-05-19 18:16:54 +0300

    touchups for main merge, removed prints

--- blade.py
+++ blade.py
@@ -2,7 +2,6 @@ from update import EssentialsUpdate
 from variables import UniversalVariables
 
 def find_number_in_list_of_lists(list_of_lists, number):
-    print('number', number)
     for row_index, sublist in enumerate(list_of_lists):
         for col_index, element in enumerate(sublist):
             if element == number:
@@ -26,13 +25,12 @@ def change_blades(self):
         closed_horizon_door_count = count_occurrences_in_list_of_lists(self.terrain_data, 989)
         closed_vertical_door_count = count_occurrences_in_list_of_lists(self.terrain_data, 9099)
 
-        print(
-            'open_horizon_door_count    989_98',    open_horizon_door_count, 
-            'open_vertical_door_count   9099_98',    open_vertical_door_count,
-            'closed_horizon_door_count  989',    closed_horizon_door_count,
-            'closed_vertical_door_count 9099',    closed_vertical_door_count
-        )
-
+        # print(
+            # 'open_horizon_door_count    989_98',    open_horizon_door_count, 
+            # 'open_vertical_door_count   9099_98',    open_vertical_door_count,
+            # 'closed_horizon_door_count  989',    closed_horizon_door_count,
+            # 'closed_vertical_door_count 9099',    closed_vertical_door_count
+        # )
 
         if EssentialsUpdate.day_night_text == 'Night':
             UniversalVariables.already_looped_blades = 'Night'
@@ -58,6 +56,5 @@ def change_blades(self):
 
             # Horisontaalsed usked tekivad
             for index in range(open_horizon_door_count):
-                print('2', index)
                 row, col = find_number_in_list_of_lists(self.terrain_data, 989_98)
                 self.terrain_data[row][col] = 989
