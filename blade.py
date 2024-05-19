8787b385e69153e796a8570c4ee80065cb3093f3
SIKASZZU <soots.sigvrid@gmail.com>
2024-05-19 18:12:38 +0300

    blade"id muutuvad closed, open

--- blade.py
+++ blade.py
@@ -2,6 +2,7 @@ from update import EssentialsUpdate
 from variables import UniversalVariables
 
 def find_number_in_list_of_lists(list_of_lists, number):
+    print('number', number)
     for row_index, sublist in enumerate(list_of_lists):
         for col_index, element in enumerate(sublist):
             if element == number:
@@ -18,31 +19,45 @@ def count_occurrences_in_list_of_lists(list_of_lists, number):
 
 
 def change_blades(self):
-    #print('already_looped', UniversalVariables.already_looped_blades, '\nEssentialsUpdate.day_night_text', EssentialsUpdate.day_night_text)
     if EssentialsUpdate.day_night_text != UniversalVariables.already_looped_blades:
-        open_door_count = count_occurrences_in_list_of_lists(self.terrain_data, 989)
-        open_closed_count = count_occurrences_in_list_of_lists(self.terrain_data, 9099)
-        for row in self.terrain_data:
+        open_horizon_door_count = count_occurrences_in_list_of_lists(self.terrain_data, 989_98)
+        open_vertical_door_count = count_occurrences_in_list_of_lists(self.terrain_data, 9099_98)
+        
+        closed_horizon_door_count = count_occurrences_in_list_of_lists(self.terrain_data, 989)
+        closed_vertical_door_count = count_occurrences_in_list_of_lists(self.terrain_data, 9099)
 
-            if EssentialsUpdate.day_night_text == 'Night':
+        print(
+            'open_horizon_door_count    989_98',    open_horizon_door_count, 
+            'open_vertical_door_count   9099_98',    open_vertical_door_count,
+            'closed_horizon_door_count  989',    closed_horizon_door_count,
+            'closed_vertical_door_count 9099',    closed_vertical_door_count
+        )
 
-                # Muudab horisontaalsed usked maze groundiks.
-                for index in range(open_door_count): 
-                    row, col = find_number_in_list_of_lists(self.terrain_data, 989)
-                    self.terrain_data[row][col] == 989_98
-                # Vertikaalsed usked tekivad
-                for index in range(open_door_count):
-                    row, col = find_number_in_list_of_lists(self.terrain_data, 9099_98)
-                    self.terrain_data[row][col] == 9099
-                UniversalVariables.already_looped_blades = 'Night'
 
-            elif EssentialsUpdate.day_night_text == 'Day':
-                # Muudab vertikaalsed usked maze groundiks.
-                for index in range(open_closed_count):
-                    row, col = find_number_in_list_of_lists(self.terrain_data, 9099)
-                    self.terrain_data[row][col] == 9099_98
-                # Horisontaalsed usked tekivad
-                for index in range(open_door_count):
-                    row, col = find_number_in_list_of_lists(self.terrain_data, 989_98)
-                    self.terrain_data[row][col] == 989
-                UniversalVariables.already_looped_blades = 'Day'
+        if EssentialsUpdate.day_night_text == 'Night':
+            UniversalVariables.already_looped_blades = 'Night'
+
+            # Muudab horisontaalsed usked maze groundiks.
+            for index in range(closed_horizon_door_count): 
+                row, col = find_number_in_list_of_lists(self.terrain_data, 989)
+                self.terrain_data[row][col] = 989_98
+            
+            # Vertikaalsed usked tekivad
+            for index in range(open_vertical_door_count):
+                row, col = find_number_in_list_of_lists(self.terrain_data, 9099_98)
+                self.terrain_data[row][col] = 9099
+
+        elif EssentialsUpdate.day_night_text == 'Day':
+            UniversalVariables.already_looped_blades = 'Day'
+
+            # Muudab vertikaalsed usked maze groundiks.
+            for index in range(closed_vertical_door_count):
+                row, col = find_number_in_list_of_lists(self.terrain_data, 9099)
+                print(self.terrain_data[row][col])
+                self.terrain_data[row][col] = 9099_98
+
+            # Horisontaalsed usked tekivad
+            for index in range(open_horizon_door_count):
+                print('2', index)
+                row, col = find_number_in_list_of_lists(self.terrain_data, 989_98)
+                self.terrain_data[row][col] = 989
