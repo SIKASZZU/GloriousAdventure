e15f0099e2072705321cddf8032019db6f636823
Oljake <olariviitak@gmail.com>
2024-06-26 15:59:19 +0300

    optimatiseerimine

--- render.py
+++ render.py
@@ -14,26 +14,9 @@ class RenderPictures:
     generated_ground_images: dict = {}
     generated_water_images: dict = {}
 
-
     def image_to_sequence(self, terrain_x: int, terrain_y: int, position: tuple[int, int], image,
                           terrain_value) -> None:
-        """
-        Teisendab pildi jadaks, kui esemel on mitu erinevat pilti, ja jaotab need positsioonide vahel laiali.
-
-        Args:
-            terrain_x (int): Grid X * block size + offset.
-            terrain_y (int): Grid Y * block size + offset.
-            position (tuple[int, int]): (Grid X, Grid Y).
-            image: Pilt, mida töödeldakse.
-            terrain_value: Väärtus, mis määrab maastiku tüübi.
-
-        Returns:
-            None
-        """
-
         if image:
-            current_entry = [image, (terrain_x, terrain_y)]
-
             occupied_positions_key = f"occupied_positions_{terrain_value}"
 
             if occupied_positions_key not in RenderPictures.__dict__:
@@ -51,19 +34,14 @@ class RenderPictures:
                 if [scaled_saved_image, (terrain_x, terrain_y)] not in UniversalVariables.blits_sequence:
                     UniversalVariables.blits_sequence.append([scaled_saved_image, (terrain_x, terrain_y)])
 
-
     def map_render(self) -> None:
-        #  NOTE:
-        #    COL == J == X
-        #    ROW == I == Y
-        #    M6tle koordinaatteljestikule. Kui X muutub, muutub column. Kui muutub Y, siis muutub row.
-
         UniversalVariables.screen.fill('white')
-        RenderPictures.terrain_in_view: list = []
+        RenderPictures.terrain_in_view.clear()
 
-        # Use the camera's position to determine the render range
-        camera_grid_row = int((Camera.camera_rect.left + Camera.camera_rect.width / 2) // UniversalVariables.block_size) - 1
-        camera_grid_col = int((Camera.camera_rect.top + Camera.camera_rect.height / 2) // UniversalVariables.block_size) - 1
+        camera_grid_row = int(
+            (Camera.camera_rect.left + Camera.camera_rect.width / 2) // UniversalVariables.block_size) - 1
+        camera_grid_col = int(
+            (Camera.camera_rect.top + Camera.camera_rect.height / 2) // UniversalVariables.block_size) - 1
 
         player_grid_x = int(UniversalVariables.player_x // UniversalVariables.block_size)
         player_grid_y = int(UniversalVariables.player_y // UniversalVariables.block_size)
@@ -74,87 +52,93 @@ class RenderPictures:
                 row_range_0, row_range_1 = player_grid_y - RenderPictures.render_range - 2, player_grid_y + RenderPictures.render_range + 4
                 col_range_0, col_range_1 = player_grid_x - RenderPictures.render_range - 3, player_grid_x + RenderPictures.render_range + 4
             else:
-                RenderPictures.render_range: int = (UniversalVariables.screen_x + UniversalVariables.screen_y) // (UniversalVariables.block_size) // 5
+                RenderPictures.render_range = (UniversalVariables.screen_x + UniversalVariables.screen_y) // UniversalVariables.block_size // 5
                 row_range_0, row_range_1 = camera_grid_col - RenderPictures.render_range, camera_grid_col + RenderPictures.render_range + 3
                 col_range_0, col_range_1 = camera_grid_row - RenderPictures.render_range - 3, camera_grid_row + RenderPictures.render_range + 6
 
             for row in range(row_range_0, row_range_1):
-                self.row: list[tuple[int, int], ...] = []
+                current_row = []
 
                 for col in range(col_range_0, col_range_1):
-                    # Kontrollib kas terrain block jääb faili terrain_data piiridesse
                     if 0 <= row < len(self.terrain_data) and 0 <= col < len(self.terrain_data[row]):
-                        self.row.append((col, row))  # 25.06.24, keerasin col ja row ymber, sest collision box listi refactorimisel l2ks midagi pekki.
+                        current_row.append((col, row))
                         terrain_value = self.terrain_data[row][col]
-                        terrain_x: int = col * UniversalVariables.block_size + UniversalVariables.offset_x
-                        terrain_y: int = row * UniversalVariables.block_size + UniversalVariables.offset_y
-                        position = (col, row)  # Using grid indices directly for the position
-                        
-                        if terrain_value == None: pass
-                        else:
-                            image = None
-                            
-                            # BACKGROUNDI LISAMINE KUHU VAJA
+                        terrain_x = col * UniversalVariables.block_size + UniversalVariables.offset_x
+                        terrain_y = row * UniversalVariables.block_size + UniversalVariables.offset_y
+                        position = (col, row)
+
+                        if terrain_value is not None:
+
+                            if terrain_value == 11:
+                                image = ImageLoader.load_image('Maze_Ground_Keyhole')
+                                RenderPictures.image_to_sequence(self, terrain_x, terrain_y, position, image, terrain_value)
+
+                            if terrain_value == 107:
+                                image = ImageLoader.load_image('Farmland')
+                                RenderPictures.image_to_sequence(self, terrain_x, terrain_y, position, image, terrain_value)
+
                             if 0 <= terrain_value <= 10:
-                                if terrain_value == 7:  
+                                image = None
+                                if terrain_value == 7:
                                     image = ImageLoader.load_image('Farmland')
-                                    RenderPictures.image_to_sequence(self,terrain_x, terrain_y, position,image, terrain_value)
-                                elif terrain_value == 10:  
+
+                                elif terrain_value == 10:
                                     image = ImageLoader.load_image('Maze_Ground_Keyhole')
-                                    RenderPictures.image_to_sequence(self,terrain_x, terrain_y, position,image, terrain_value)
+
                                 else:
-                                    image_name = 'Ground_' + str(random.randint(0, 19)) if terrain_value != 0 else 'Water_0'
+                                    image_name = 'Ground_' + str(
+                                        random.randint(0, 19)) if terrain_value != 0 else 'Water_0'
                                     image = ImageLoader.load_image(image_name)
 
-                                    # Ground pildile eraldi render, et see asi ei muutuks objekti eemaldamisel
-                                    if position not in RenderPictures.occupied_positions:
+                                if position not in RenderPictures.occupied_positions:
+                                    scaled_image = pygame.transform.scale(image, (
+                                    UniversalVariables.block_size, UniversalVariables.block_size))
+                                    if [scaled_image, (terrain_x, terrain_y)] not in UniversalVariables.blits_sequence:
+                                        UniversalVariables.blits_sequence.append([scaled_image, (terrain_x, terrain_y)])
+                                    RenderPictures.occupied_positions[position] = scaled_image
+                                else:
+                                    scaled_image = RenderPictures.occupied_positions[position]
+                                    if [scaled_image, (terrain_x, terrain_y)] not in UniversalVariables.blits_sequence:
+                                        UniversalVariables.blits_sequence.append([scaled_image, (terrain_x, terrain_y)])
 
-                                        scaled_image = pygame.transform.scale(image, (UniversalVariables.block_size, UniversalVariables.block_size))
-                                        if [scaled_image,(terrain_x, terrain_y)] not in UniversalVariables.blits_sequence:
-                                            UniversalVariables.blits_sequence.append([scaled_image,(terrain_x, terrain_y)])
+                            elif terrain_value == 1000:
+                                image = ImageLoader.load_image('Final_Maze_Ground_2')
+                                RenderPictures.image_to_sequence(self, terrain_x, terrain_y, position, image, terrain_value)
 
-                                        RenderPictures.occupied_positions[position] = scaled_image
-                                    else:
-                                        scaled_image = RenderPictures.occupied_positions[position]
-                                        scaled_saved_image = pygame.transform.scale(scaled_image, (UniversalVariables.block_size, UniversalVariables.block_size))
+                            elif terrain_value in {933, 977}:
+                                if EssentialsUpdate.day_night_text == 'Night':
+                                    self.terrain_data[row][col] = 977
+                                    image = ImageLoader.load_image('Maze_End_Bottom')
+                                else:
+                                    self.terrain_data[row][col] = 933
+                                RenderPictures.image_to_sequence(self, terrain_x, terrain_y, position, image, terrain_value)
 
-                                        if [scaled_image,(terrain_x, terrain_y)] not in UniversalVariables.blits_sequence:
-                                            UniversalVariables.blits_sequence.append([scaled_saved_image,(terrain_x, terrain_y)])
+                            elif terrain_value == 98:
+                                image = ImageLoader.load_image('Maze_Ground')
+                                RenderPictures.image_to_sequence(self, terrain_x, terrain_y, position, image,
+                                                                 terrain_value)
 
-                            elif terrain_value == 1000:  
-                                image = ImageLoader.load_image('Final_Maze_Ground_2')  # MAZE GROUND BACKGROUNDI LISAMINE
-                                RenderPictures.image_to_sequence(self,terrain_x, terrain_y, position,image, terrain_value)
-
-                            elif terrain_value == 933 or terrain_value == 977:
-                                # mu aju ytleb, et see elif lihtsalt 2ra kustutada, sest allpool on juba see, et laeb k6ik objekti id'de pealt sise
-                                # aga kui seda elifi ei ole, siis 66sel uksed kinni ei l2he, idk miks
-
-                                if EssentialsUpdate.day_night_text == 'Night':  
-                                    self.terrain_data[position[1]][position[0]] = 977
-                                    
-                                    image = ImageLoader.load_image('Maze_End_Bottom')  # MAZE GROUND BACKGROUNDI LISAMINE
-                                    RenderPictures.image_to_sequence(self,terrain_x, terrain_y, position,image, terrain_value)
-                                else:  
-                                    self.terrain_data[position[1]][position[0]] = 933
-                                
-                            # Spawnib maze ground, wall ja vist veel asju, mdea.
-                            else:
-                                for item in items_list:
-                                    if terrain_value == item.get('ID'):
-                                        image_name = item.get('Name')
-                                     
-                                # special case, sest walle on meil 9 erinevat tykki.
-                                if image_name.startswith('Maze_Wall'): image_name = 'Maze_Wall_' + str(random.randint(0,9))
-                    
+                            elif terrain_value == 99:
+                                image_name = 'Maze_Wall_' + str(random.randint(0, 9))
                                 image = ImageLoader.load_image(image_name)
-                                RenderPictures.image_to_sequence(self, terrain_x, terrain_y, position,image, terrain_value)            
+                                RenderPictures.image_to_sequence(self, terrain_x, terrain_y, position, image,
+                                                                 terrain_value)
 
-                RenderPictures.terrain_in_view.append(self.row)
+
+                            else:
+                                image_name = next((item['Name'] for item in items_list if terrain_value == item['ID']),
+                                                  None)
+                                if image_name:
+                                    if image_name.startswith('Maze_Wall'):
+                                        image_name = 'Maze_Wall_' + str(random.randint(0, 9))
+                                    image = ImageLoader.load_image(image_name)
+                                    RenderPictures.image_to_sequence(self, terrain_x, terrain_y, position, image, terrain_value)
+
+                RenderPictures.terrain_in_view.append(current_row)
             UniversalVariables.screen.blits(UniversalVariables.blits_sequence, doreturn=False)
 
         except IndexError:
             return
-
 
     def object_render():
         for item in UniversalVariables.object_list:
